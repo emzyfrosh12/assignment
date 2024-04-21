fa743a25cb8614fc58575fcd089c2acb5d243e62
emzyfrosh12 <167190608+emzyfrosh12@users.noreply.github.com>
2024-04-21 14:01:26 +0200

    Add files via upload

--- tutorial_php/kkk.php
+++ tutorial_php/kkk.php
@@ -3,26 +3,61 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
     <title>Document</title>
 </head>
 <body>
 
 <?php
 
-'$object = ('Emmanuel joseph', 'Mark zuckberg','Jack grealish');
-
-foreach ($object as $show){
-
-    <div class="card">
-    <div class="card-border">
-<h1>
-echo .$show;
-</h1>
-   </div>
-    </div>
+$object = ['Emmanuel joseph', 'Mark zuckberg','Jack grealish'];
 
 
-}'
+foreach ($object as $show)
+
+
+
+
+echo ' <table class="container">
+<table class="table table-striped table-hover">
+
+<th>1</th>
+<th>2</th>
+<th>3</th>
+
+
+
+
+<tr>
+<td>'.$show.'</td>
+</tr>
+
+
+
+
+
+
+
+</table>
+</table>
+
+';
+
+
+
+
+
+// echo  '<div class="card">
+//     <div class="card-border">
+// <h1>
+//  '.'My name is ' .$show.'
+// </h1>
+//    </div>
+//     </div>
+
+
+// ';
 
 ?>
     
